de211867fa489d3ccc2cc4ad8fd29929281273eb
SmallSigh <martijnsmit4@gmail.com>
2025-04-24 13:36:24 +0200

    Removed display_prompt function and replaced newline with prompt. Fixes bug that deletes the prompt

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: masmit <masmit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 15:47:55 by masmit            #+#    #+#             */
-/*   Updated: 2025/04/22 15:52:31 by masmit           ###   ########.fr       */
+/*   Updated: 2025/04/24 13:33:06 by masmit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,6 @@
 void	cleanup_wip(void)
 {
 	exit(EXIT_FAILURE);
-}
-
-void	display_prompt(void)
-{
-	write(1, "minishell$ ", 11);
 }
 
 int	main(int argc, char **argv, char **env)
@@ -32,8 +27,7 @@ int	main(int argc, char **argv, char **env)
 	(void)env;
 	while (true)
 	{
-		display_prompt();
-		input = readline("");
+		input = readline("minishell$ ");
 		if (syntax_check(input) == FAILURE)
 		{
 			printf("syntax error\n");
