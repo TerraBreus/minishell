65c0b8ffe07743e606a30b982de4616baa2a111a
SmallSigh <martijnsmit4@gmail.com>
2025-05-05 16:57:28 +0200

    moved while(true) loop to seperate function called loop to save space in main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: masmit <masmit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 15:47:55 by masmit            #+#    #+#             */
-/*   Updated: 2025/04/24 13:33:06 by masmit           ###   ########.fr       */
+/*   Updated: 2025/05/05 14:14:58 by masmit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,39 @@ void	ctrl_d(char *input)
 		exit(EXIT_SUCCESS);
 }
 
+void	loop(t_custom_env *my_env)
+{
+	char	*input;
+	t_list	*token_list;
+
+	token_list = NULL;
+	input = readline("minishell$ ");
+	ctrl_d(input);
+	if (*input == '\0')
+		free(input);
+	else
+	{
+		token_list = tokenize_input(input);
+		free(input);
+	}
+	if (token_list)
+	{
+		print_tokens(token_list);
+		token_expansion(token_list, my_env);
+		ft_lstclear(&token_list, token_del);
+	}
+}
 
 int	main(int argc, char **argv, char **env)
 {
 	t_custom_env	*my_env;
-	t_list			*token_list;
-	char			*input;
 
 	(void)argc;
 	(void)argv;
-	my_env = shell_env_init(env);
-	if (!my_env)
+	if ((my_env = shell_env_init(env)) == NULL)
 		return (EXIT_FAILURE);
 	while (true)
-	{
-		input = readline("minishell$ ");
-		ctrl_d(input);
-		if (*input == '\0')
-		{
-			free(input);
-			continue;
-		}
-		token_list = tokenize_input(input);
-		free(input);
-		if (token_list)
-		{
-			print_tokens(token_list);
-			token_expansion(token_list, my_env);
-			printf("---token list after expansion---\n");
-			print_tokens(token_list);
-			ft_lstclear(&token_list, token_del);
-		}
-	}
+		loop(my_env);
 	cleanup_env(my_env);
 	return (0);
 }
