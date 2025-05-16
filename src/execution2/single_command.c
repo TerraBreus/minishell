c4913af6a6ce6f0c4d22b05d80a64c30dc1db2ea
Zacharia Ivanov <zivanov@f1r4s14.codam.nl>
2025-05-16 16:54:24 +0200

    removing irrelevant files

--- src/execution2/single_command.c
+++
@@ -1,9 +0,0 @@
-int	single_command(t_cmd *cmd_list, t_custom_envp *t_envp)
-{
-//Setup redirection.
-	if (set_redirection(cmd_list->redirection) == -1)
-		return (-1);
-	if (exec_cmd(cmd_list->argv, t_envp) == -1)
-		return (-1);
-	return (0);
-}
