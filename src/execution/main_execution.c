ec56e4f7cd8a577a009e155b75c8628e2b825e84
Zacharia Ivanov <zivanov@f1r4s14.codam.nl>
2025-05-16 16:57:09 +0200

    rewrote the whole function to correctly restore and save standard IO

--- src/execution/main_execution.c
+++ src/execution/main_execution.c
@@ -1,93 +1,27 @@
 #include "minishell.h"
 
-typedef struct s_pipe
+int	chain_of_commands(t_cmd *cmd_list, t_custom_env *t_envp)
 {
-	int 	pfd[2];
-	int	last_read_end;
-}	t_pipe;
+	int	cmd_c;
+	int	error_check;
 
-void	call_plumber(t_pipe **plumber)
-{
-	if (*plumber == NULL)
-	{
-		*plumber = (t_pipe *)malloc(sizeof(t_pipe));
-		if (*plumber == NULL)
-			exit(EXIT_FAILURE);				//TODO
-	}
-	pipe((*plumber)->pfd);
-}
+	//TODO
+	//print some error message saying there must have been prior malloc failure.
+	if (cmd_list == NULL || t_envp == NULL)
+		return (-1);
 
-void	parental_control(t_cmd *cmd_list, t_custom_env *t_envp, t_pipe *plumber)
-{
-	static bool	first_time = true;
+	cmd_c = count_commands(cmd_list);
+	
+	//COPY STDIN and STDOUT to restore at the end.
+	save_or_restore_io(SAVE);	//TODO
 
-	if (first_time == true)
-	{
-		//close	write side
-		//save	readside of pipe to last_read_end.
-	}
-	else if (cmd_list->next != NULL)
-	{
-		//close	last_read_end
-		//close	write side pipe (p_fd[1])
-		//save	read side of pipe (p_fd[0])
-	}
+	//MULTIPLE COMMANDS WILL CREATE A PIPE STRUCTURE. SINGLE_COMMAND WONT.
+	if (cmd_c == 1)
+		error_check = single_command(cmd_list, t_envp);
 	else
-	{
-		//close	last_read_end
-	}
+		error_check = multiple_commands(cmd_list, t_envp);	//TODO
+
+	//RESTORE THE STDIN AND STDOUT TO ALLOW FOR NEW PROMPT AND USER INPUT. 
+	save_or_restore_IO(RESTORE);
+	return (error_check);
 }
-
-void	child_slavery(t_cmd *cmd_list, t_custom_env *t_envp, t_pipe *plumber)
-{
-	static bool	first_time = true;
-
-	if (first_time == true)						//First child
-	{
-		//dup2	write side pipe (pfd[1]) to stdin
-		//close	whole pipe
-		first_time = false;
-	}
-	else if (cmd_list->next != NULL)				//Middle child
-	{
-		//dup2	last_read_end with stdin
-		//close	last_read_end
-		//dup2	write side pipe (pfd[1]) to stdin
-		//close	whole pipe
-	}
-	else
-	{
-		//dup2	last_read_end with stdin
-		//close	last_read_end
-		first_time = true;
-	}
-
-}
-
-int	exec_cmd_list(t_cmd *cmd_list, t_custom_env *t_envp)	//WORKS ONLY FOR NON BUILT IN COMMANDS!!! TODO
-{
-	pid_t		pid;
-	t_pipe		*plumber;
-
-	plumber = NULL;
-	while (cmd_list != NULL)
-	{
-		if (cmd_list->next != NULL)
-			call_plumber(&plumber);				//CREATE NEW PIPE
-
-		pid = fork();
-		if (pid == 0)
-		{
-			child_slavery(cmd_list, t_envp, plumber);
-			setup_io(cmd_list->redirection);
-			exec_cmd_in_child(cmd_list->argv, t_envp->env_copy, plumber->last_read_end);
-		}
-		else
-			parental_control(cmd_list, t_envp, plumber);
-		cmd_list = cmd_list->next;
-	}
-	while (wait(NULL) != -1)
-		;	//TODO Check erro for no_child
-	return (0);
-}
-
