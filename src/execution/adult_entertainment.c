db16d2838c9410d58ab37859a2a67fe97f52cea9
Zacharia Ivanov <zivanov@f0r2s4.codam.nl>
2025-05-19 17:30:48 +0200

    added pipe handling for parent

--- src/execution/adult_entertainment.c
+++ src/execution/adult_entertainment.c
@@ -1,8 +1,14 @@
 #include "minishell.h"
 
-int	adult_entertainment(t_cmd *cmd_list, t_custom_env *t_envp)
+int	adult_entertainment(t_cmd *cmd_list, t_custom_env *t_envp, t_pipe *pipe_data)
 {
-	//Currently have nothing for this function.
+	if (pipe_data->last_read_end != -1)
+		close(pipe_data->last_read_end);
+	if (cmd_list->next != NULL)
+	{
+		pipe_data->last_read_end = pipe_data->pfd[1];
+		close(pipe_data->pfd[0]);
+	}
 	(void)cmd_list;
 	(void)t_envp;
 	return (0);
