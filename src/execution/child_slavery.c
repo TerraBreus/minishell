c8169506b89483022b41a9bffa0f4d3336b008f5
Zacharia Ivanov <zivanov@f0r2s4.codam.nl>
2025-05-19 17:31:15 +0200

    added pipehandling for children

--- src/execution/child_slavery.c
+++ src/execution/child_slavery.c
@@ -2,9 +2,29 @@
 
 void	child_slavery(t_cmd *cmd_list, t_custom_env *t_envp, t_pipe *plumb_box)
 {
-	if (plumb_box->last_read_end != -1)
-		close(plumb_box->last_read_end);
-	
+	static bool	first_cmd = true;
+
+	if (cmd_list->next != NULL)
+	{
+		if (first_cmd == true)
+		{
+			first_cmd = false;
+			if (setup_pipe_first_cmd(plumb_box) == -1)
+				exit(EXIT_FAILURE);	//TODO
+		}
+		else
+		{
+			if (setup_pipe_middle_cmd(plumb_box) == -1)
+				exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		first_cmd = true;
+		if (setup_pipe_last_cmd(plumb_box) == -1)
+			exit(EXIT_FAILURE);
+	}
+
 	save_or_restore_io(CLOSE);
 	//SOLELY FOR DEBUGGING!!!
 	for (int fd = 0; fd < 1024; ++fd)
