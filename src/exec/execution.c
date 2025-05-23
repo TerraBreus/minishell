01d6263919734ad0eb002bacb0572bedd951a5ad
Zacharia Ivanov <zivanov@f1r7s3.codam.nl>
2025-05-23 13:02:27 +0200

    added initial execution flow

--- src/exec/execution.c
+++ src/exec/execution.c
@@ -1,9 +1,12 @@
-#include "minishell.h"
 
+/*
 static void	no_cmd_given(void)
 {
 	//print error message.
 }
+*/
+
+#include "minishell.h"
 
 void	execution(t_cmd *cmd_list, t_shell *shell_data)
 {
@@ -11,13 +14,15 @@ void	execution(t_cmd *cmd_list, t_shell *shell_data)
 
 	save_close_restore_io(SAVE);
 	cmd_c = count_commands(cmd_list);
-	if (cmd_c < 1)
-		no_cmd_given();				//TODO NULL pointer given.
+	if (cmd_c < 2)					//TODO Remove when you have more options
+		return ;
 	/*
+	if (cmd_c < 1)
+		no_cmd_given();				//TODO what to do when NULL pointer given?
 	else if (cmd_c == 1)
 		;//single_command(cmd_list, shell_data);	//TODO
 	else
-		; //mult_commands(cmd_list, shell_data);	//TODO
+		; //mult_commands(cmd_list, shell_data);	//IN PROGRESS
 	*/
 	mult_cmd(cmd_list, shell_data);
 	save_close_restore_io(RESTORE);
