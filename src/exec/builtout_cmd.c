15cffaf8ca79a8c45136d24b74f4510b3a8420ed
Zacharia Ivanov <zivanov@f1r7s3.codam.nl>
2025-05-23 13:01:32 +0200

    fixed function if no systemcalls fail

--- src/exec/builtout_cmd.c
+++ src/exec/builtout_cmd.c
@@ -10,17 +10,17 @@ int	builtout_cmd(t_cmd *cmd_list, t_shell *shell_data, t_pipe *pipe_data)
 		exit(EXIT_FAILURE);				//TODO Forking failure.
 	if (pipe_data != NULL)
 	{
-		if (setup_pipe_builtout(pipe_data) == -1)	//TODO
+		if (setup_pipe_builtout(pipe_data, pid, cmd_list->type) == -1)
 			exit(EXIT_FAILURE);			//TODO dup2 failure.
 	}
-	if (setup_redir == -1)					//TODO
-		exit(EXIT_FAILURE);				//dup2 failure
+	if (setup_redir(cmd_list->redirection) == -1)
+		exit(EXIT_FAILURE);				//TODO dup2 failure
 	if (pid == 0)
 	{
-		exec_cmd(cmd_list, shell_data->env_copy);	//TODO
+		exec_cmd(cmd_list->argv, shell_data->env_copy);
 		return (-1);
 	}
 	else
 		return (0);
-
+}
 
