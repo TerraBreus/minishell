5fed15224acde5e9589e6899dc024cd5d7a9b896
Zacharia Ivanov <zivanov@f1r7s2.codam.nl>
2025-05-26 09:49:23 +0200

    added function when prompt has only 1 command

---
+++ src/exec/single_cmd.c
@@ -0,0 +1,14 @@
+#include "minishell.h"
+
+int	single_cmd(t_cmd *cmd_list, t_shell *shell_data)
+{
+	if (is_built_in(cmd_list) == true)
+		builtin_cmd(shell_data, cmd_list);
+	else
+	{
+		if (builtout_cmd(cmd_list, shell_data, NULL) == -1)
+			exit(EXIT_FAILURE);				//TODO
+		wait(NULL);						//TODO: retrieve exit status of child.
+	}
+	return (0);
+}
