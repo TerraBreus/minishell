f8faa5e05738f75fcd4c0b3bea61e89f7df3a89a
Zacharia Ivanov <zivanov@f1r7s2.codam.nl>
2025-05-26 09:49:44 +0200

    added isbuiltin that returns if functions is built in

---
+++ src/exec/is_built_in.c
@@ -0,0 +1,31 @@
+#include "minishell.h"
+
+bool	is_built_in(t_cmd *cmd_list)
+{
+	t_cmd	*current;
+
+	current = cmd_list;
+	if (ft_strncmp(current->argv[0], "echo", 5) == 0)
+		return (true);
+	else if (ft_strncmp(current->argv[0], "cd", 3) == 0)
+		return (true);
+	else if (ft_strncmp(current->argv[0], "pwd", 4) == 0)
+		return (true);
+	else if (ft_strncmp(current->argv[0], "export", 7) == 0)
+		return (true);
+	else if (ft_strncmp(current->argv[0], "env", 4) == 0)
+		return (true);
+	/*
+	else if (current->argv[0][0] == '/')
+		dir_unknown(shell);
+		return (true);
+	*/
+	else if (ft_strncmp(current->argv[0], "unset", 4) == 0)
+		return (true);
+	/*
+	else if (ft_strncmp(current->argv[0], "exit", 5) == 0)
+		my_exit(shell, *exec);
+	*/
+	else
+		return (false);
+}
