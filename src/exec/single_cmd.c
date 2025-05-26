8d45639f72ad5e61484879811b42f42cdf00b437
Zacharia Ivanov <zivanov@f1r7s2.codam.nl>
2025-05-26 14:00:54 +0200

    fixed small compilation error

--- src/exec/single_cmd.c
+++ src/exec/single_cmd.c
@@ -3,7 +3,7 @@
 int	single_cmd(t_cmd *cmd_list, t_shell *shell_data)
 {
 	if (is_built_in(cmd_list) == true)
-		builtin_cmd(shell_data, cmd_list);
+		builtin_cmd(shell_data, cmd_list, NULL);
 	else
 	{
 		if (builtout_cmd(cmd_list, shell_data, NULL) == -1)
