576325e38d17c833f4e33b3b4d462898b249c6e9
Zacharia Ivanov <zivanov@f1r7s2.codam.nl>
2025-05-30 11:38:01 +0200

    Undid martijn's function for single_cmd cause it does not set up pipes/redirection

--- src/exec/single_cmd.c
+++ src/exec/single_cmd.c
@@ -1,18 +1,19 @@
 #include "minishell.h"
 
-// int	single_cmd(t_cmd *cmd_list, t_shell *shell_data)
-// {
-// 	if (is_built_in(cmd_list) == true)
-// 		builtin_cmd(shell_data, cmd_list, NULL);
-// 	else
-// 	{
-// 		if (builtout_cmd(cmd_list, shell_data, NULL) == -1)
-// 			exit(EXIT_FAILURE);				//TODO
-// 		wait(NULL);						//TODO: retrieve exit status of child.
-// 	}
-// 	return (0);
-// }
+int	single_cmd(t_cmd *cmd_list, t_shell *shell_data)
+{
+	if (is_built_in(cmd_list) == true)
+		builtin_cmd(shell_data, cmd_list, NULL);
+	else
+	{
+		if (builtout_cmd(cmd_list, shell_data, NULL) == -1)
+			exit(EXIT_FAILURE);				//TODO
+		wait(NULL);						//TODO: retrieve exit status of child.
+	}
+	return (0);
+}
 
+/*
 int	single_cmd(t_cmd *cmd_list, t_shell *shell_data)
 {
 	if (builtin(shell_data, &cmd_list) == BUILTIN_NOT_FOUND)
@@ -23,3 +24,4 @@ int	single_cmd(t_cmd *cmd_list, t_shell *shell_data)
 	}
 	return (0);
 }
+*/
