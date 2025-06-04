cee73342e4c7559274f5ae4a520999c2fd932076
Zacharia Ivanov <zivanov@f1r5s3.codam.nl>
2025-06-04 17:05:49 +0200

    Usage of is_built_in to correctly set up pipes when function is actually built out
    Mostly cause I am lazy and want to reuse code I already wrote instead of
    rewriting everything

--- src/exec/mult_cmd.c
+++ src/exec/mult_cmd.c
@@ -14,11 +14,14 @@ int	mult_cmd(t_cmd *cmd_list, t_shell *shell_data)
 			if (create_pipe(&pipe_data) == -1)
 				exit(EXIT_FAILURE);								//TODO Close pipe_struct and exit on malloc/pipe failure
 		}
-		last_pid = builtin(shell_data, &cmd_list);
-		if (last_pid == BUILTIN_NOT_FOUND)
+		if (is_built_in(cmd_list) == true)
+		{
+			last_pid = builtin_cmd(shell_data, cmd_list, &pipe_data);
+			if (cmd_list->next == NULL)
+				return (last_pid); //in this case we must return the exit status of the built in command.
+		}
+		else
 			last_pid = builtout_cmd(cmd_list, shell_data, &pipe_data);
-		else if (cmd_list->next == NULL)
-			return (last_pid); //in this case we must return the exit status of the built in command.
 		cmd_list = cmd_list->next;
 	}
 	return (ft_wait(last_pid, &status));
