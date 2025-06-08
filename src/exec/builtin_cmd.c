2c61d1f444b706b9cbb77339729cb21db2804e86
SmallSigh <martijnsmit4@gmail.com>
2025-06-08 14:54:46 +0200

    removed dir not found and cmd not found, as both are done by execve

--- src/exec/builtin_cmd.c
+++ src/exec/builtin_cmd.c
@@ -14,22 +14,6 @@
 //  and 7 to create holy bash
 #include "minishell.h"
 
-static void	cmd_unknown(t_shell *shell)
-{
-	write(STDERR_FILENO, shell->tokens[0], ft_strlen(shell->tokens[0]));
-	write(STDERR_FILENO, ": Command not found\n", 21);
-	shell->last_errno = SYNTAX_ERROR;
-	shell->found_error = true;
-}
-
-static void	dir_unknown(t_shell *shell)
-{
-	write(STDERR_FILENO, shell->tokens[0], ft_strlen(shell->tokens[0]));
-	write(STDERR_FILENO, ": Is a directory\n", 18);
-	shell->last_errno = DIR_PROMPT;
-	shell->found_error = true;
-}
-
 static void	my_exit(t_shell *shell, t_cmd *exec)
 {
 	cleanup_env(shell);
@@ -53,14 +37,10 @@ void	exec_builtin(t_shell *shell, t_cmd *exec)
 		my_export(shell, current->argv);
 	else if (ft_strncmp(current->argv[0], "env", 4) == 0)
 		my_env(shell);
-	else if (current->argv[0][0] == '/')
-		dir_unknown(shell);
 	else if (ft_strncmp(current->argv[0], "unset", 4) == 0)
 		my_unset(shell, current->argv);
 	else if (ft_strncmp(current->argv[0], "exit", 5) == 0)
 		my_exit(shell, exec);
-	else
-		cmd_unknown(shell);
 }
 
 int	builtin_cmd(t_shell *shell, t_cmd *cmd_list, t_pipe *pipe_data)
