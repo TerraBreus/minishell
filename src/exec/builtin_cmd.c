e8f9515e40cf024f820c493a2061baa325d75dd9
SmallSigh <martijnsmit4@gmail.com>
2025-06-10 19:52:28 +0200

    removed unused code

--- src/exec/builtin_cmd.c
+++ src/exec/builtin_cmd.c
@@ -12,35 +12,6 @@
 
 #include "minishell.h"
 
-static void	my_exit(t_shell *shell, t_cmd *exec)
-{
-	cleanup_env(shell);
-	cleanup_shell(shell);
-	cleanup_struct(&exec);
-	exit(EXIT_SUCCESS);
-}
-
-void	exec_builtin(t_shell *shell, t_cmd *exec)
-{
-	t_cmd	*current;
-
-	current = exec;
-	if (ft_strncmp(current->argv[0], "echo", 5) == 0)
-		my_echo(current->argv);
-	else if (ft_strncmp(current->argv[0], "cd", 3) == 0)
-		my_cd(shell, current->argv);
-	else if (ft_strncmp(current->argv[0], "pwd", 4) == 0)
-		my_pwd(shell);
-	else if (ft_strncmp(current->argv[0], "export", 7) == 0)
-		my_export(shell, current->argv);
-	else if (ft_strncmp(current->argv[0], "env", 4) == 0)
-		my_env(shell);
-	else if (ft_strncmp(current->argv[0], "unset", 4) == 0)
-		my_unset(shell, current->argv);
-	else if (ft_strncmp(current->argv[0], "exit", 5) == 0)
-		my_exit(shell, exec);
-}
-
 int	builtin_cmd(t_shell *shell, t_cmd *cmd_list)
 {
 	if (setup_redir(cmd_list->redirection) == -1)
