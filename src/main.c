54bb2a79735b657797037cde3fbb0350c2c4c10a
SmallSigh <martijnsmit4@gmail.com>
2025-06-10 19:35:42 +0200

    removed old comments

--- src/main.c
+++ src/main.c
@@ -12,35 +12,6 @@
 
 #include "minishell.h"
 
-/*
- * heredoc + CTRL-C currently exits instead of creating new prompt.
- *
-*/
-
-// ''""'' persmission denied
-// echo 'hi'" "'hello' error: syntax error near unexpected token `UNCLOSED QUOTE
-// hello shd be Command 'hello' not found and exit code 127
-// argument after exit should be translated to exit code eg
-// exit 188; echo $?; 188 --this number can overflow 9223372036854775807
-//my_shell: cat | cat | ls, cat: -: Bad file descriptor, cat: closing standard input: Bad file descriptor
-// cat shd stay open. -> wait for every child finish, not only last pipe
-// sleep 3 | ls, ours works left to right, bash works concurrent --> FIXED(?)
-
-// dfdf | fdf Syscall param close(fd) contains uninitialised byte(s)
-// echo hi > permission (this permission has no right to do anything), program write hi into permission and quit program, exit code again
-// echo HI >> 1 >> 2 >> 3 not append HI in file 3
-// cat << eof
-// > hi
-// > ctrl-c definitely lost 1 from run_heredoc, ft_strdup
-// if heredoc with several empty lines, direct leaks
-// delimitor with quote, shdnt expand
-// ctrl c noq quicks shell
-// >> is overwrite instead append
-// cat << eof > file | cat file, should not cat the file
-// echo hi | echo "&" do nothing
-//cat < hello  didnt check the file is exist or not
-//nest minishell, signal handling
-// echo + - !
 int	main(void)
 {
 	t_shell	shell;
