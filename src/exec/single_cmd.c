f8c0984bb5c18a73b56c031034cce45e771c2462
SmallSigh <martijnsmit4@gmail.com>
2025-06-11 16:04:02 +0200

    single cmd returns on not having an arg list

--- src/exec/single_cmd.c
+++ src/exec/single_cmd.c
@@ -6,7 +6,7 @@
 /*   By: masmit <masmit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 17:25:32 by zivanov           #+#    #+#             */
-/*   Updated: 2025/06/06 16:41:38 by masmit           ###   ########.fr       */
+/*   Updated: 2025/06/11 16:03:40 by masmit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	single_cmd(t_cmd *cmd_list, t_shell *shell_data)
 	int	pid;
 	int	status;
 
+	if (!cmd_list->argv)
+		return (0);
 	if (is_built_in(cmd_list) == true)
 		return (builtin_cmd(shell_data, cmd_list));
 	else
