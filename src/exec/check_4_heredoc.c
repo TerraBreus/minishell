221c9445d870badf4204ac758660785c1ddf1d18
SmallSigh <martijnsmit4@gmail.com>
2025-06-13 18:09:45 +0200

    commented out function otherwise Makefile errors

--- src/exec/check_4_heredoc.c
+++ src/exec/check_4_heredoc.c
@@ -12,23 +12,23 @@
 
 #include "minishell.h"
 
-int	check_4_heredoc(t_shell *shell, t_cmd *cmd_list)
-{
-	t_redir	*temp;
+// int	check_4_heredoc(t_shell *shell, t_cmd *cmd_list)
+// {
+// 	t_redir	*temp;
 
-	while (cmd_list != NULL)
-	{
-		temp = cmd_list->redirection;
-		while (temp != NULL)
-		{
-			if (temp->type == HEREDOC)
-			{
-				if (setup_heredoc(shell, temp, cmd_list) == -1)
-					return (-1);
-			}
-			temp = temp->next;
-		}
-		cmd_list = cmd_list->next;
-	}
-	return (0);
-}
+// 	while (cmd_list != NULL)
+// 	{
+// 		temp = cmd_list->redirection;
+// 		while (temp != NULL)
+// 		{
+// 			if (temp->type == HEREDOC)
+// 			{
+// 				if (setup_heredoc(shell, temp, cmd_list) == -1)
+// 					return (-1);
+// 			}
+// 			temp = temp->next;
+// 		}
+// 		cmd_list = cmd_list->next;
+// 	}
+// 	return (0);
+// }
