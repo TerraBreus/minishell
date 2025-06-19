fde70ae57d390801a33cf9b3b3023c9f0feffe35
Zacharia Ivanov <zivanov@f1r2s10.codam.nl>
2025-06-19 16:54:36 +0200

    refactor extra functin for norminette

--- src/exec/store_heredoc.c
+++ src/exec/store_heredoc.c
@@ -1,45 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         ::::::::           */
+/*   store_heredoc.c                                     :+:    :+:           */
+/*                                                      +:+                   */
+/*   By: zivanov <marvin@42.fr>                        +#+                    */
+/*                                                    +#+                     */
+/*   Created: 2025/06/19 16:13:29 by zivanov        #+#    #+#                */
+/*   Updated: 2025/06/19 16:13:30 by zivanov        ########   odam.nl        */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+int	return_first_fd(int buffer[], int i)
+{
+	int	result;
+
+	while (buffer[i] == -3)
+		i++;
+	result = buffer[i];
+	buffer[i] = -3;
+	return (result);
+}
+
+void	clean_buffer(int buffer[], int i, bool *first_time)
+{
+	while (++i < 200)
+		buffer[i] = -2;
+	*first_time = false;
+}
 
 int	store_heredoc(int pipe_read)
 {
 	static int	buffer[200];
 	static bool	first_time = true;
-	int		i;
-	int		result;
+	int			i;
 
 	if (pipe_read == CLOSE)
 		first_time = true;
 	else if (pipe_read > 0)
 	{
 		if (first_time == true)
-		{
-			i = -1;
-			while (++i < 200)
-				buffer[i] = -2;
-			first_time = false;
-		}
+			clean_buffer(buffer, -1, &first_time);
 		i = 0;
 		while (buffer[i] != -2)
 			i++;
 		buffer[i] = pipe_read;
 	}
 	else
-	{
-		i = 0;
-		while (buffer[i] == -3)
-			i++;
-		result = buffer[i];
-		buffer[i] = -3;
-		return (result);
-	}
+		return (return_first_fd(buffer, 0));
 	return (0);
 }
 
 void	close_heredoc_parent(t_redir *r)
 {
 	int	fd;
-	
-	while(r != NULL)
+
+	while (r != NULL)
 	{
 		if (r->type == HEREDOC)
 		{
@@ -49,4 +66,3 @@ void	close_heredoc_parent(t_redir *r)
 		r = r->next;
 	}
 }
-
