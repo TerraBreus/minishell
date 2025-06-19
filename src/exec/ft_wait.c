df73d8660494cab2528d81b9a60ca32aacf9614c
Zacharia Ivanov <zivanov@f1r2s10.codam.nl>
2025-06-19 16:55:26 +0200

    norminette refactor

--- src/exec/ft_wait.c
+++ src/exec/ft_wait.c
@@ -1,37 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_wait.c                                          :+:      :+:    :+:   */
+/*   ft_wait.c                                           :+:    :+:           */
 /*                                                    +:+ +:+         +:+     */
 /*   By: masmit <masmit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 17:23:23 by zivanov           #+#    #+#             */
-/*   Updated: 2025/06/07 14:56:21 by masmit           ###   ########.fr       */
+/*   Updated: 2025/06/19 16:26:50 by zivanov        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_wait(t_shell *shell, int last_pid, int *status)
+static void	wait_and_check_errno(void)
 {
-	if (last_pid > 0)
-	{
-		waitpid(last_pid, status, 0);
-		while (wait(NULL) != -1)
-			;
-		if (errno != ECHILD)
-		{
-			perror("ft_wait: ");
-			exit(EXIT_FAILURE);
-		}
-		if (WIFSIGNALED(*status) && WTERMSIG(*status) == SIGINT)
-		{	
-			ft_putchar_fd('\n', STDERR_FILENO);
-			shell->last_errno = 130;
-		}
-		else if (WIFEXITED(*status) != 0)
-			shell->last_errno = WEXITSTATUS(*status);
-	}
 	while (wait(NULL) != -1)
 		;
 	if (errno != ECHILD)
@@ -39,5 +21,22 @@ int	ft_wait(t_shell *shell, int last_pid, int *status)
 		perror("ft_wait: ");
 		exit(EXIT_FAILURE);
 	}
+}
+
+int	ft_wait(t_shell *shell, int last_pid, int *status)
+{
+	if (last_pid > 0)
+	{
+		waitpid(last_pid, status, 0);
+		wait_and_check_errno();
+		if (WIFSIGNALED(*status) && WTERMSIG(*status) == SIGINT)
+		{
+			ft_putchar_fd('\n', STDERR_FILENO);
+			shell->last_errno = 130;
+		}
+		else if (WIFEXITED(*status) != 0)
+			shell->last_errno = WEXITSTATUS(*status);
+	}
+	wait_and_check_errno();
 	return (1);
 }
